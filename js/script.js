13ecb0c57da053278d63154f31837d67afe7b2a5
thaqnos <thaqnos@gmail.com>
2019-09-25 16:50:18 -0300

    refatora codigo

--- js/script.js
+++ js/script.js
@@ -1,28 +1,26 @@
 let formulario = document.getElementById("formulario-de-comentarios")
+let todosComentarios = document.getElementById("todos-os-comentarios")
+let mensagemInvalida = document.getElementById("mensagem-invalida")
 
 formulario.addEventListener("submit", function(evento){
     //preventDefault() - previne ações default do submit (que é um evento) que é recarregar a página
     evento.preventDefault()
 
-    //captar o elemento de input para guardar a mensagem
     let inputForm = document.getElementById("input-formulario")
+    let mensagem = inputForm.value // pegar valor do input
     
-    //pegar o >valor< do input fornecido pelo usuário e guardar em uma variável
-    let mensagem = inputForm.value
-
-    // pegar a div 
-    let divComentarios = document.getElementById("todos-os-comentarios")
-
-    // criar um >parágrafo<
-    let comentarios = document.createElement("p")
-
-    // adicionar estilo ao comentário
-    comentarios.classList.add("comentario")
-
-    // colocar o parágrafo que eu criei como filho da div
-    divComentarios.appendChild(comentarios)
-    
-    // colocar o conteúdo na outra div
-    comentarios.textContent = mensagem
-
+    if (mensagem.trim() === ""){
+        // metrodo.trim() serve para remover todo o espaço das pontas anterior e posterior da frase
+        inputForm.classList.add("comentario-invalido")
+        mensagemInvalida.textContent = "Por favor, insira um comentário"
+        mensagemInvalida.classList.add("texto-invalido")
+    } else {
+        mensagemInvalida.textContent = ""
+        let comentarios = document.createElement("p")
+        comentarios.textContent = mensagem        
+        todosComentarios.appendChild(comentarios)    
+        comentarios.classList.add("comentario")    
+        formulario.reset() 
+        // resetar o formulario - resetar o input ao estado inicial        
+    }
 })
